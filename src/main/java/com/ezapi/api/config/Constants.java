ca901aa2762776ef55a97f2f9edd2c3bf609e2f4
Prafulm1234 <121489217+Prafulm1234@users.noreply.github.com>
2023-01-13 16:25:21 +0530

    pushing files from API

---
+++ src/main/java/com/ezapi/api/config/Constants.java
@@ -0,0 +1,11 @@
+package com.ezapi.api.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
